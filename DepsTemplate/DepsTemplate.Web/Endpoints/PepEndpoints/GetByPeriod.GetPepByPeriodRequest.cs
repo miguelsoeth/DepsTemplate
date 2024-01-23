ccd4e786e0e30298d799f97b897735d0024aacd5
Miguel Soeth <miguelsoeth@gmail.com>
2024-01-23 17:57:06 -0300

    end of day 23/01/24

--- DepsTemplate/DepsTemplate.Web/Endpoints/PepEndpoints/GetByPeriod.GetPepByPeriodRequest.cs
+++ DepsTemplate/DepsTemplate.Web/Endpoints/PepEndpoints/GetByPeriod.GetPepByPeriodRequest.cs
@@ -8,15 +8,10 @@ namespace DepsTemplate.Web.Endpoints.PepEndpoints
 {
     public class GetPepByPeriodRequest
     {
+        public const string Route = "peps/{PerfilId:int}";
+        public static string BuildRoute(int perfilId) => Route.Replace("{PerfilId:int}", perfilId.ToString());
         public string? Cpf { get; set; }
-        public string? Nome { get; set; }
-        public string? DescricaoFuncao { get; set; }
-        public string? NomeOrgao { get; set; }
         public string? DataInicioExercicio { get; set; }
         public string? DataFimExercicio { get; set; }
-        public string? CodigoOrgao { get; set; }
-        public string? SiglaFuncao { get; set; }
-        public string? NivelFuncao { get; set; }
-        public string? DataFimCarencia { get; set; }
     }
 }
--- DepsTemplate/DepsTemplate.Web/Endpoints/PepEndpoints/GetByPeriod.cs
+++ DepsTemplate/DepsTemplate.Web/Endpoints/PepEndpoints/GetByPeriod.cs
@@ -1,5 +1,7 @@
 ﻿using Ardalis.ApiEndpoints;
+using DepsTemplate.Core.Entities.PerfilAggregate;
 using DepsTemplate.Core.Interfaces;
+using DepsTemplate.SharedKernel.Interfaces;
 using DepsTemplate.Web.Endpoints.PerfilEndpoints;
 using DepsTemplate.Web.Filters;
 using Microsoft.AspNetCore.Authorization;
@@ -15,65 +17,29 @@ using System.Threading.Tasks;
 namespace DepsTemplate.Web.Endpoints.PepEndpoints
 {
     [ApiVersion("1.0")]
-    [ApiController]
-    [Route("api/v{version:apiVersion}/pep")]
+    [Route("api/v{version:apiVersion}")]
     [AllowAnonymous]
-    [EnableCors("AllowSpecificOrigin")]
-    public class GetByPeriod : ControllerBase
+    public class GetByPeriod : EndpointBaseAsync.WithRequest<GetPepByPeriodRequest>.WithActionResult<GetPepByPeriodResponse>
     {
-        public readonly IPepService _pepService;
+        private readonly IPepService _pepService;
 
         public GetByPeriod(IPepService pepService)
         {
             _pepService = pepService;
         }
-        [HttpGet]
+
+        [HttpGet(GetPepByPeriodRequest.Route)]
         [SwaggerOperation(
-          Summary = "Obtém um pep por periodo",
-          OperationId = "Peps.GetByPeriod",
+          Summary = "Obtém pep por periodo",
+          OperationId = "Pep.GetByPeriod",
           Tags = new[] { "PepEndpoints" })
         ]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-
-        public async Task<ActionResult> PepConsultaPorCPF([FromQuery][Required][RegularExpression("^[0-9]*$")] string cpf, [FromQuery][RegularExpression(@"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$")] string periodoInicial, [FromQuery][RegularExpression(@"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$")] string periodoFinal)
+        public override async Task<ActionResult<GetPepByPeriodResponse>> HandleAsync(GetPepByPeriodRequest request, CancellationToken cancellationToken = default)
         {
-            if (!VerifyCpf.IsValid(cpf))
-            {
-                return BadRequest("CPF inválido");
-            }
+            var entity = await _pepService.ConsultaPep(request.Cpf, request.DataInicioExercicio, request.DataFimExercicio);
+            if (entity == null) return NotFound();
 
-            var response = await _pepService.ConsultaPep(cpf, periodoInicial, periodoFinal);
-            if (response.CodigoHttp == HttpStatusCode.OK)
-            {
-                return Ok(response.DadosRetorno);
-            }
-            else
-            {
-                return StatusCode((int)response.CodigoHttp, response.ErroRetorno);
-            }
+            return Ok(entity);
         }
     }
 }
-/*
-{
-    [ApiVersion("1.0")]
-    [Route("api/v{version:apiVersion}")]
-    [AllowAnonymous]
-    public class GetByPeriod : EndpointBaseAsync.WithRequest<GetPepByPeriodRequest>.WithActionResult<GetPepByPeriodResponse>
-    {
-        [HttpGet(GetPerfilByIdRequest.Route)]
-        [SwaggerOperation(
-          Summary = "Obtém um perfil por id",
-          OperationId = "Perfil.GetById",
-          Tags = new[] { "PerfilEndpoints" })
-        ]
-        public override Task<ActionResult<GetPepByPeriodResponse>> HandleAsync(GetPepByPeriodRequest request, CancellationToken cancellationToken = default)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-}
-*/
